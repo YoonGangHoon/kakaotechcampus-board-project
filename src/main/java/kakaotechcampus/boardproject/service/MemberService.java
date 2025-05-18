a8c822235c88a0bcd5e919ec24dbf6c39b86d9b4
YoonGangHoon <yghun021007@naver.com>
2025-05-18 22:11:36 +0900

    Refactor: 사용하지 않는 라이브러리 제거

--- src/main/java/kakaotechcampus/boardproject/service/MemberService.java
+++ src/main/java/kakaotechcampus/boardproject/service/MemberService.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.beans.Transient;
 import java.util.Optional;
 
 @Service
